d8cc8cd2e9d81127dd0fabf99a5bb5fe05167d03
Damian Tasevski <damian@revdebug.com>
2019-03-30 05:08:18 +0100

    added taking pictures

--- ObywatelPlus/ObywatelPlus.Android/MainActivity.cs
+++ ObywatelPlus/ObywatelPlus.Android/MainActivity.cs
@@ -6,18 +6,24 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
+using Plugin.Media;
+using Plugin.Permissions;
 
 namespace ObywatelPlus.Droid
 {
     [Activity(Label = "ObywatelPlus", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override async void OnCreate(Bundle bundle)
         {
             //TabLayoutResource = Resource.Layout.Tabbar;
             //ToolbarResource = Resource.Layout.Toolbar; 
 
             base.OnCreate(bundle);
+
+            await CrossMedia.Current.Initialize();
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
@@ -30,6 +36,11 @@ namespace ObywatelPlus.Droid
             {
                 Toast.MakeText(ApplicationContext, "U", ToastLength.Short).Show();
             });
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
